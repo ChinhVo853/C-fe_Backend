a6539d960690d5c467425ed5a1ab5f1a96ad6fcc
ChinhVo853 <chinhvo853@gmail.com>
2024-06-13 15:01:37 +0700

    nguoi_dung

---
+++ app/Models/NguoiDung.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Tymon\JWTAuth\Contracts\JWTSubject;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class NguoiDung extends Authenticatable implements JWTSubject
+{
+    use HasFactory;
+    protected $table = "nguoi_dung";
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
+}
